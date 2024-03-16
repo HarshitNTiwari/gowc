df2d21129ee945a6800cce606a3cd06cc312e6f9
Harshit Tiwari <20uec059@lnmiit.ac.in>
2024-03-17 01:24:08 +0530

    Update help messages for the options

--- gowc.go
+++ gowc.go
@@ -15,10 +15,10 @@ func main() {
 		charVar  bool
 	)
 
-	flag.BoolVar(&bytesVar, "c", false, "count no. of bytes in the file")
-	flag.BoolVar(&linesVar, "l", false, "count no. of lines in the file")
-	flag.BoolVar(&wordsVar, "w", false, "count no. of words in the file")
-	flag.BoolVar(&charVar, "m", false, "count no. of chars in the file")
+	flag.BoolVar(&bytesVar, "c", false, "Count the number of bytes in the input file")
+	flag.BoolVar(&linesVar, "l", false, "Count the number of new line characters in the input file")
+	flag.BoolVar(&wordsVar, "w", false, "Count the number of words in the input file")
+	flag.BoolVar(&charVar, "m", false, "Count the number of characters in the input file")
 	flag.Parse()
 
 	filenames := flag.Args()
